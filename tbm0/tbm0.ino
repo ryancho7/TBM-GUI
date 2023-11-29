3ccc72c42b2f1d1a98f397da45f1ac5dd18f38a7
nordgren137 <129317714+nordgren137@users.noreply.github.com>
2023-11-29 15:19:28 -0800

    Add files via upload

---
+++ tbm0/tbm0.ino
@@ -0,0 +1,29 @@
+// Wheel pins
+int leftWheel1 = 2;
+int leftWheel2 = 3;
+int rightWheel1 = 4;
+int rightWheel2 = 7;
+int wheelENA = 5;
+int wheelENB = 6;
+// Cutterhead pins
+int cut1 = 8;
+int cut2 = 11;
+int cutENA = 9;
+
+void setup() {
+  pinMode(leftWheel1, OUTPUT);
+  pinMode(leftWheel2, OUTPUT);
+  pinMode(rightWheel1, OUTPUT);
+  pinMode(rightWheel2, OUTPUT);
+  pinMode(wheelENA, OUTPUT);
+  pinMode(wheelENB, OUTPUT);
+  pinMode(cut1, OUTPUT);
+  pinMode(cut2, OUTPUT);
+  pinMode(cutENA, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
